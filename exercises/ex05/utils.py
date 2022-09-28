92492497cf3b6f5cbc57e1f113f0655bded0be60
Arun Somasundaram <arunrsoma@gmail.com>
2022-09-28 17:51:59 -0400

    Finished Exercise 5

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,4 +1,4 @@
-"""Writing code to be unit tested"""
+"""Writing code to be unit tested."""
 __author__ = "730509674"
 
 
@@ -13,7 +13,9 @@ def only_evens(input: list[int]) -> list[int]:
 
 def concat(first_list: list[int], second_list: list[int]) -> list[int]:
     """This function takes two given lists annd combines them to make one list."""
-    final_list: list[int] = first_list
+    final_list: list[int] = []
+    for number in first_list:
+        final_list.append(number)
     for number in second_list:
         final_list.append(number)
     return final_list
@@ -27,15 +29,9 @@ def sub(a_list: list[int], start: int, end: int) -> list[int]:
     if start < 0:
         start = 0
     if end > len(a_list):
-        end = len(a_list) - 1
+        end = len(a_list)
     i = start
     while i < end:
         end_list.append(a_list[i])
         i += 1
     return end_list
-
-    
-    
-
-
-
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -4,37 +4,47 @@ __author__ = "730509674"
 
 from exercises.ex05.utils import only_evens, sub, concat
 
+
 def test_only_evens_empty() -> None:
+    """Testing the function with an empty list."""
     assert only_evens([]) == []
 
 
 def test_only_evens_normal_list() -> None:
-    assert only_evens([1, 2, 3, 4, 5, 6, 7, 8 , 9 ,10]) == [2, 4, 6, 8, 10]
+    """Testing the function with a normal list with even and odd integers."""
+    assert only_evens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == [2, 4, 6, 8, 10]
 
 
 def test_only_evens_odd_list() -> None:
+    """Testing the function with a list of only odd integers."""
     assert only_evens([1, 3, 5, 7, 9]) == []
 
 
 def test_concat_two_empty_lists() -> None:
+    """Testing if the function returns an empty list with two empty lists at the beginning."""
     assert concat([], []) == []
 
 
 def test_concat_normal_lists() -> None:
+    """Testing if the function returns a combined list with the integers from both of the lists."""
     assert concat([1, 2, 30, 115], [3, 80, 25]) == [1, 2, 30, 115, 3, 80, 25]
 
 
 def test_concat_more_normal_lists() -> None:
-    assert concat([34, 50],[3, 90]) == [34, 50, 3, 90]
+    """Testing if the function returns a combined list with the integers from both of the lists."""
+    assert concat([], [3, 90]) == [3, 90]
 
 
+def test_sub_large_end() -> None:
+    """Testing to see if the function reassigns the end value when it is larger than the length of the list, so that the end value is the last value in the list."""
+    assert sub([1, 4, 7], -40, 4) == [1, 4, 7]
 
-def test_sub_small_end() -> None:
-    assert sub([1, 4, 7], -40, 0) == []
 
 def test_sub_normal_list() -> None:
+    """Testing to see if the function returns a new list with values from the start value up to the end value."""
     assert sub([5, 8, 9, 10, 356], 1, 5) == [8, 9, 10, 356]
 
-def test_sub_another_normal_list() -> None:
-    assert sub([1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6], 4, 9) == [5, 6, 7, 8, 1]
 
+def test_sub_another_normal_list() -> None:
+    """Testing to see if the function returns a new list with values from the start value up to the end value."""
+    assert sub([1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6], 4, 9) == [5, 6, 7, 8, 1]
